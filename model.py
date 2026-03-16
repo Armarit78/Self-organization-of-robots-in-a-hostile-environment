56cd497033806a1d1da68705217f308e07bac847
Christophe Boshra <christophe.boshra@student-cs.fr>
2026-03-16 15:36:37 +0100

    add agent action in model

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import mesa
 import random
 from objects import Radioactivity, WasteDisposalZone, Waste
-# from agents import GreenRobot, YellowRobot, RedRobot # A faire
+from agents import greenAgent, yellowAgent, redAgent
 
 # Fonctions pour le DataCollector
 def compute_green_wastes(model):
@@ -19,7 +19,7 @@ def compute_red_wastes(model):
 
 
 class RobotMission(mesa.Model):
-    def __init__(self, width=15, height=10, initial_wastes=10):
+    def __init__(self, width=15, height=10, initial_wastes=10, nb_green=5, nb_yellow=5, nb_red=5):
         super().__init__()
         self.grid = mesa.space.MultiGrid(width, height, torus=False)
         self.schedule = mesa.time.RandomActivation(self)
@@ -67,6 +67,29 @@ class RobotMission(mesa.Model):
             self.schedule.add(waste)
             
         # 4. Ajouter les robots
+        # Le robot vert ne peut pas dépasser la zone z1 
+        for _ in range(nb_green):
+            robot = greenAgent(self.next_id(), self)
+            x = random.randrange(z1_max) # Placement uniquement dans z1
+            y = random.randrange(height)
+            self.grid.place_agent(robot, (x, y))
+            self.schedule.add(robot)
+
+        # Le robot jaune peut se déplacer dans les zones z1 et z2 
+        for _ in range(nb_yellow):
+            robot = yellowAgent(self.next_id(), self)
+            x = random.randrange(z2_max) # Placement dans z1 ou z2
+            y = random.randrange(height)
+            self.grid.place_agent(robot, (x, y))
+            self.schedule.add(robot)
+
+        # Le robot rouge peut se déplacer dans les zones z1, z2 et z3 
+        for _ in range(nb_red):
+            robot = redAgent(self.next_id(), self)
+            x = random.randrange(width) # Placement n'importe où sur la grille
+            y = random.randrange(height)
+            self.grid.place_agent(robot, (x, y))
+            self.schedule.add(robot)
 
         self.running = True
 
